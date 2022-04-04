47c4bce314d59cbf797103894419c4bdd9cfed77
DENISSP3KTR <54879762+DENISSP3KTR@users.noreply.github.com>
2022-04-04 15:35:15 +0900

    design main form

---
+++ optimizator/optimizator/mainform.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace optimizator
+{
+    partial class mainform
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainform));
+            this.closeBtn = new System.Windows.Forms.PictureBox();
+            this.resizeBtn = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.closeBtn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resizeBtn)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // closeBtn
+            // 
+            this.closeBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.closeBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.closeBtn.Image = ((System.Drawing.Image)(resources.GetObject("closeBtn.Image")));
+            this.closeBtn.InitialImage = global::optimizator.Properties.Resources.closeicon2;
+            this.closeBtn.Location = new System.Drawing.Point(946, 0);
+            this.closeBtn.Name = "closeBtn";
+            this.closeBtn.Size = new System.Drawing.Size(25, 25);
+            this.closeBtn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.closeBtn.TabIndex = 0;
+            this.closeBtn.TabStop = false;
+            this.closeBtn.Click += new System.EventHandler(this.closeBtn_Click);
+            this.closeBtn.MouseEnter += new System.EventHandler(this.closeBtn_MouseEnter);
+            this.closeBtn.MouseLeave += new System.EventHandler(this.closeBtn_MouseLeave);
+            // 
+            // resizeBtn
+            // 
+            this.resizeBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.resizeBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.resizeBtn.Image = ((System.Drawing.Image)(resources.GetObject("resizeBtn.Image")));
+            this.resizeBtn.InitialImage = global::optimizator.Properties.Resources.closeicon2;
+            this.resizeBtn.Location = new System.Drawing.Point(915, 0);
+            this.resizeBtn.Name = "resizeBtn";
+            this.resizeBtn.Size = new System.Drawing.Size(25, 25);
+            this.resizeBtn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.resizeBtn.TabIndex = 1;
+            this.resizeBtn.TabStop = false;
+            this.resizeBtn.Click += new System.EventHandler(this.resizeBtn_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.panel1.Controls.Add(this.resizeBtn);
+            this.panel1.Controls.Add(this.closeBtn);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(971, 27);
+            this.panel1.TabIndex = 2;
+            // 
+            // mainform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(971, 523);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "mainform";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.mainform_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.closeBtn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resizeBtn)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox closeBtn;
+        private System.Windows.Forms.PictureBox resizeBtn;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ optimizator/optimizator/mainform.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace optimizator
+{
+    public partial class mainform : Form
+    {
+        public mainform()
+        {
+            InitializeComponent();
+        }
+        Image closeicon1 = Image.FromFile("D:/github/optimizatorbyspektr/img/closeicon.png");
+        Image closeicon2 = Image.FromFile("D:/github/optimizatorbyspektr/img/closeicon2.png");
+        public int state = 0;
+        private void closeBtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+
+        }
+        private void mainform_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void closeBtn_MouseEnter(object sender, EventArgs e)
+        {
+            closeBtn.Image = closeicon2;
+        }
+
+        private void closeBtn_MouseLeave(object sender, EventArgs e)
+        {
+            closeBtn.Image = closeicon1;
+        }
+
+        private void resizeBtn_Click(object sender, EventArgs e)
+        {
+            if(state == 0)
+            {
+                WindowState = FormWindowState.Maximized;
+                state = 1;
+            }
+            else
+            {
+                WindowState = FormWindowState.Normal;
+                state = 0;
+            }
+        }
+    }
+}
