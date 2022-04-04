a04c6c6babdd65576b4664581838de277c26743d
DENISSP3KTR <54879762+DENISSP3KTR@users.noreply.github.com>
2022-04-04 17:05:50 +0900

    design main form

--- optimizator/optimizator/mainform.Designer.cs
+++ optimizator/optimizator/mainform.Designer.cs
@@ -34,23 +34,24 @@ namespace optimizator
             this.resizeBtn = new System.Windows.Forms.PictureBox();
             this.panel1 = new System.Windows.Forms.Panel();
             this.collapseBtn = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             ((System.ComponentModel.ISupportInitialize)(this.closeBtn)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.resizeBtn)).BeginInit();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.collapseBtn)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.tabControl1.SuspendLayout();
             this.SuspendLayout();
             // 
             // closeBtn
             // 
-            this.closeBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            resources.ApplyResources(this.closeBtn, "closeBtn");
             this.closeBtn.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.closeBtn.Image = ((System.Drawing.Image)(resources.GetObject("closeBtn.Image")));
             this.closeBtn.InitialImage = global::optimizator.Properties.Resources.closeicon2;
-            this.closeBtn.Location = new System.Drawing.Point(946, 0);
             this.closeBtn.Name = "closeBtn";
-            this.closeBtn.Size = new System.Drawing.Size(25, 25);
-            this.closeBtn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.closeBtn.TabIndex = 0;
             this.closeBtn.TabStop = false;
             this.closeBtn.Click += new System.EventHandler(this.closeBtn_Click);
             this.closeBtn.MouseEnter += new System.EventHandler(this.closeBtn_MouseEnter);
@@ -58,15 +59,10 @@ namespace optimizator
             // 
             // resizeBtn
             // 
-            this.resizeBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            resources.ApplyResources(this.resizeBtn, "resizeBtn");
             this.resizeBtn.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.resizeBtn.Image = ((System.Drawing.Image)(resources.GetObject("resizeBtn.Image")));
             this.resizeBtn.InitialImage = global::optimizator.Properties.Resources.closeicon2;
-            this.resizeBtn.Location = new System.Drawing.Point(915, 0);
             this.resizeBtn.Name = "resizeBtn";
-            this.resizeBtn.Size = new System.Drawing.Size(25, 25);
-            this.resizeBtn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.resizeBtn.TabIndex = 1;
             this.resizeBtn.TabStop = false;
             this.resizeBtn.Click += new System.EventHandler(this.resizeBtn_Click);
             // 
@@ -76,44 +72,63 @@ namespace optimizator
             this.panel1.Controls.Add(this.collapseBtn);
             this.panel1.Controls.Add(this.resizeBtn);
             this.panel1.Controls.Add(this.closeBtn);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
+            resources.ApplyResources(this.panel1, "panel1");
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(971, 27);
-            this.panel1.TabIndex = 2;
             // 
             // collapseBtn
             // 
-            this.collapseBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            resources.ApplyResources(this.collapseBtn, "collapseBtn");
             this.collapseBtn.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.collapseBtn.Image = ((System.Drawing.Image)(resources.GetObject("collapseBtn.Image")));
             this.collapseBtn.InitialImage = global::optimizator.Properties.Resources.closeicon2;
-            this.collapseBtn.Location = new System.Drawing.Point(884, 0);
             this.collapseBtn.Name = "collapseBtn";
-            this.collapseBtn.Size = new System.Drawing.Size(25, 25);
-            this.collapseBtn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.collapseBtn.TabIndex = 2;
             this.collapseBtn.TabStop = false;
             this.collapseBtn.Click += new System.EventHandler(this.collapseBtn_Click);
             // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.tabControl1);
+            resources.ApplyResources(this.panel2, "panel2");
+            this.panel2.Name = "panel2";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            resources.ApplyResources(this.tabControl1, "tabControl1");
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.DimGray;
+            resources.ApplyResources(this.tabPage1, "tabPage1");
+            this.tabPage1.Name = "tabPage1";
+            // 
+            // tabPage2
+            // 
+            resources.ApplyResources(this.tabPage2, "tabPage2");
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
             // mainform
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.ClientSize = new System.Drawing.Size(971, 523);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.HelpButton = true;
             this.Name = "mainform";
             this.ShowIcon = false;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
             this.Load += new System.EventHandler(this.mainform_Load);
             ((System.ComponentModel.ISupportInitialize)(this.closeBtn)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.resizeBtn)).EndInit();
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.collapseBtn)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -124,6 +139,10 @@ namespace optimizator
         private System.Windows.Forms.PictureBox resizeBtn;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.PictureBox collapseBtn;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
     }
 }
 
